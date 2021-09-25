80aeb4734a09070e830598d8ed198bf5ce823602
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-09-25 13:56:44 -0500

    Se creo la clase RegistroAspirantes

--- src/PruebaMemoriaDinamica.java
+++ src/PruebaMemoriaDinamica.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
-
+import java.util.Iterator;
+import java.util.ArrayList;
 
 class Aspirante{
 	private int folio;
@@ -50,6 +51,41 @@ class Aspirante{
 	
 	
 }
+
+
+class RegistroAspirantes {
+	
+	ArrayList listaAspirantes;
+	private int nFolio=1;
+	
+	public int getnFolio() {
+		return nFolio;
+	}
+	public void setnFolio(int nFolio) {
+		this.nFolio = nFolio;
+	}
+	
+	public RegistroAspirantes() {
+		listaAspirantes = new ArrayList();
+	}
+	public void agregarAspirante(Aspirante a) {
+		this.setnFolio(this.getnFolio()+1);
+		listaAspirantes.add(a);
+	}
+	public Object eliminarAspirante(String folio) {
+		this.setnFolio(this.getnFolio()-1);
+		return listaAspirantes.remove(listaAspirantes.size()-1);
+	}
+	public void mostrarAspirantes() {
+		Iterator i = listaAspirantes.iterator();
+		while(i.hasNext()) {
+			Aspirante a = (Aspirante) i.next();
+			System.out.println(a);
+		}
+	}	
+}
+
+
 
 public class PruebaMemoriaDinamica {
 
